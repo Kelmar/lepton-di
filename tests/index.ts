1195adede07bdd325519b5fc3d18ebd59f891ac6
Kelmar Firesun <kfiresun@gmail.com>
2019-01-04 12:47:14 -0600

    Removing old comment in tests.

--- tests/index.ts
+++ tests/index.ts
@@ -5,18 +5,4 @@ import { runAllTests } from './decorators';
 import './injection_tests';
 import './scope_tests';
 
-/*
-@testFixture()
-class ArrayTests
-{
-    @testCase(4)
-    @testCase(5)
-    @test
-    public myTest(value: number): void
-    {
-        assert.equal([1,2,3].indexOf(value), -1);
-    }
-}
-*/
-
 runAllTests();
